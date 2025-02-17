5909d6bb9d06b25ae641f861f6cea15e0fd7fe78
bhroston <51910844+bhroston@users.noreply.github.com>
2025-02-17 11:41:52 -0600

    [docs] Revise fcmconfr docs with latest edits Add references for fcmconfr Add comments describing different IVFN subtraction methods in subtract_ivfn function

--- R/fcmconfr.R
+++ R/fcmconfr.R
@@ -15,68 +15,92 @@
 #'
 #' @description
 #' This is the primary function of the fcmconfr package. This function performs
-#' up to three different analyses of a set of (or an individual) input FCM(s).
-#' Use \code{\link{fcmconfr_gui}} to help select fcmconfr parameters using an
-#' interactive GUI.
+#' the following three analyses on input fuzzy cognitive maps (FCMs).
 #'
 #' \enumerate{
-#'    \item FCM Simulation: Simulate the (raw/unmodified) input FCMs to estimate
-#'          their inferences. Inferences are calculated by comparing the simulation
-#'          output of a particular scenario with the simulation output of the
-#'          baseline (i.e. the natural behavior of the network without external
-#'          perturbation), (Ozesmi & Ozesmi, 2003) and help distinguish
-#'          decision-making impacts with the structural or
-#'          expected, steady-state of the system.
-#'    \item Aggregate Analysis: Generate an aggregate adj. matrix from a list of
-#'          adj. matrices. FCM aggregation works by calculating the mean/median
-#'          edge weight for all edges across the input adj. matrices (i.e. the
-#'          mean/median of the edge weight connecting A -> B across all maps,
-#'          the mean/median of the edge weight connecting B -> C across all
-#'          maps, and so on) (Aminpour et al., 2020). The user may dictate
-#'          whether to incorporate 0-valued edge weights in the mean/median
-#'          calculations. See \code{\link{aggregate_fcms}} for more
-#'          information on aggregating FCMs outside of the
-#'          primary \code{\link{fcmconfr}} function.
-#'    \item Monte Carlo Analysis: Generate empirical adj. matrices whose edge
-#'          weights are drawn from edge weights from the input FCMs using
-#'          monte carlo sampling methods.These empirical FCMs are then simulated
-#'          in bulk using the FCM Simulation method previously described. This
-#'          outputs a dataframe of inferences across all empirical FCMs that
-#'          represent the possibility space of inferences representative of the
-#'          collective. This function also uses bootstrapping methods to
-#'          estimate confidence intervals about the inferences generated from
-#'          the empirical FCMs (may be toggled off if wanting to reduce runtime).
+#'    \item Dynamic Simulation: The dynamic behvaior of one or more input FCMs
+#'    is evaluated in response to a perturbation. Two types of simulations can
+#'    be performed, pulse and clamped.
+#'    \itemize{
+#'        \item Pulse: In pulse simulations one or more nodes are transiently
+#'        perturbed (the scenario) and allowed to relax back to equilibrium
+#'        over multiple iterations to understand the effect those nodes have
+#'        on the network. Scenarios are established using the initial state
+#'        vector (non-zero values perturb a node). All values in the clamping
+#'        vector must be set to zero (ADD REFERENCE HERE)
+#'        \item Clamped: In clamped simulations, one or more nodes of interest
+#'        are continuously activated (the scenario) and the result is compared
+#'        to baseline conditions. Scenarios are established using the clamping
+#'        vector (non-zero values clamp a node) and the baseline is
+#'        established by setting all nodes in the initial state vector to one
+#'        (i.e., it represents the response of the network to a transient
+#'        pulse event involving all nodes) (Ozesmi & Ozesmi, 2003).
+#'    }
+#'    The final response of each node of an FCM in a scenario-of-interest is
+#'    known as an inference. Inferences can be calculated using the maximum
+#'    (peak) or the final resting state (final) of a node. In pulse simulations
+#'    inferences are the difference between either of these two measures and
+#'    zero. In clamped simulations inferences are the difference between either
+#'    of these measures in the scenario-of-interest and their equivalent values
+#'    in the baseline simulation. Simulations can be run using different
+#'    activation functions (Kosko, Modified-Kosko, or Rescale) and squashing
+#'    functions (Sigmoid, Tanh)
+#'
+#'    \item Model Aggregation: Generates a single "collective" adjacency matrix
+#'    from a list of adjacency matrices. Aggregation is performed by calculating
+#'    the mean/median edge weight for all edges included in a set of input FCMs
+#'    (i.e. the mean/median of the edge weight connecting A->B across all maps,
+#'    the mean/median of the edge weight connecting B->C across all maps, and
+#'    so on) (Aminpour et al., 2020). The user specifies whether 0-valued edge
+#'    weights (reflecting the absence of a connection between two nodes)
+#'    shouldbe included when calculating the mean/median.
+#'
+#'    \emphasize{Aggregate analysis can be toggled off to reduce runtime.}
+#'
+#'    \item Monte Carlo Analysis: This method assesses uncertainty in dynamic
+#'    simulations by generating a distribution of possible inferences. Edge
+#'    weights are randomly sampled fro input FCMs to construct a set of \eqn{N}
+#'    Monte Carlo FCMs. Each FCM undergoes dyamic simulation, producing \eqn{N}
+#'    inferences per node. These inferences represent the range of possible
+#'    values each node may assume, providing a way to quantify uncertainty.
+#'
+#'    Min, Max, Median, and 25th/75th quantiles are estimated for each node.
+#'    Bootstrapping (optional) can also be performed to estimat 95% confidence
+#'    bounds about the mean or median inference for each node.
+#'
+#'    \emphasize{Monte Carlo analysis can be toggled off to reduce runtime.}
 #' }
 #'
-#' This function accepts three different types of FCMs which differ in how they
+#' \code{fcmconfr} can accommodate three types of FCMs. Note that each FCM type
+#' must be analyzed separately (i.e. they cannot be co-evaluated in a single
+#' input set). The three supported types of FCMs include:
+#'
+#' \enumerate{
+#'    \item Conventional FCMs, where edge weights are single-valued fuzzy
+#'    numbers (Stylios, 1997).
+#'    \item Interval-Valued Fuzzy Number (IVFN) FCMs, which are an extension
+#'    of conventional FCMs. IVFN-FCMs represent edge weights as ranges
+#'    [min, max], where any value within the range is as likely as any other
+#'    (i.e., the probability distribution for each edge is uniform)
+#'    (Moore & Lodwick, 2003; Hajek & Prochazka, 2016); and
+#'    \item Triangular Fuzzy Number (TFN) FCMs, which are an extension of
+#'    IVFN-FCMs. TFN-FCMs also represent edge weights as ranges, but assume
+#'    that one value within the range (the mode) is more likely than any other
+#'    (i.e., the probability distribution for each edge is triangular)
+#'    (Yesil et al., 2014).
+#' }
+#'
+#'  accepts three different types of FCMs which differ in how they
 #' represent edge weights. Note: All input FCMs must be the same type (i.e. a
 #' list of input FCMs must all be of one type or another, but cannot contain
 #' multiple types of FCMs in the same input set).
-#'
-#' \enumerate{
-#'    \item Conventional FCMs: These represent edge weights as fuzzy numbers and
-#'          represent FCMs in thir traditional form (Stylios, 1997)
-#'    \item Interval-Valued Fuzzy Number (IVFN) FCMs: An extension of Conventional
-#'          FCMs, these represent edge weights as interval-valued fuzzy numbers
-#'          (IVFNs) which describe a range of values and are defined by a lower
-#'          and upper bound (e.g. [0.2, 0.8] represents the set of values
-#'          between 0.2 and 0.8 which may be represented as a uniform distribution
-#'          via runif(N, min = 0.2, max = 0.8)) (Moore & Lodwick, 2003)
-#'    \item Triangular Fuzzy Number (TFN) FCMs: An extension of Conventional
-#'          FCMs, these represent edge weights as triangular fuzzy numbers (TFNs)
-#'          which describe a bounded range of values with a greater density of
-#'          values at some point in between, such that the probability density
-#'          function of the distribution appears triangular (Moore & Lodwick, 2003).
-#'          These are defined by their lower and upper bounds as well as a mode
-#'          value, and can be called within this package via the
-#'          \code{\link{rtriangular_dist}} function or with other packages
-#'          such as EnvStats::rtri().
-#' }
 #'
 #' @references \insertRef{ozesmiParticipatoryApproachEcosystem2003}{fcmconfr}
 #' @references \insertRef{aminpourWisdomStakeholderCrowds2020}{fcmconfr}
 #' @references \insertRef{styliosIntroducingTheoryFuzzy1997}{fcmconfr}
 #' @references \insertRef{mooreIntervalAnalysisFuzzy2003}{fcmconfr}
+#' @references \insertRef{hajek_interval-valued_2016}{fcmconfr}
+#' @references \insertRef{yesil_triangular_2014}{fcmconfr}
 #'
 #' @details
 #' Call \code{\link{fcmconfr_gui}}. for assistance with inputs.
